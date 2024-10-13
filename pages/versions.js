4ed108b26d3922a94e137841640dde3a1dfb4911
Ahmed Said Metwally <ametwally@al-watania.com>
2024-10-13 08:01:05 +0300

    first commit12

--- pages/versions.js
+++ pages/versions.js
@@ -7,8 +7,9 @@ function Versions() {
             <li className='p-2 text-green-500'>V1 : calculate mrp Without any special specifications </li>
             <li className='p-2 text-green-500'>V2 : Add special items like mixed parts</li>
             {/* <li className='p-2 '>V3 : Add special items like Americana and Albaik etc ...</li> */}
-            <li className='p-2'>V3 : possibility of calculation requirements according to multiple types of yield (cobb, Meyn, etc .. ) </li>
-            <li className='p-2'>V4 : Optimization User Experience </li>
+            <li className='p-2'>V3 : Integrate With Copilot Tool to Inhance Sales Order </li>
+            <li className='p-2'>V4 : possibility of calculation requirements according to multiple types of yield (cobb, Meyn, etc .. ) </li>
+            <li className='p-2'>V5 : Optimization User Experience </li>
         </ul>
 
     </div>
